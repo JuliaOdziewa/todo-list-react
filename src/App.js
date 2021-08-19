e719f1e16346849f1fdd8abaa6da22e8501f9e52
Julia Odziewa <julia.odziewa@gmail.com>
2021-08-19 23:22:06 +0200

    adding new task interaction

--- src/App.js
+++ src/App.js
@@ -38,11 +38,21 @@ function App() {
     })));
   };
 
+  const addNewTask = (content) => {
+    setTasks(tasks => [
+      ...tasks, {
+        content,
+        done: false,
+        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+      },
+    ])
+  };
+
   return (
     <Container>
       <Header title="Lista zadań" />
       <Section title="Dodaj nowe zdanie"
-        body={<Form />}
+        body={<Form addNewTask={addNewTask} />}
       />
       <Section
         title="Lista zadań"
