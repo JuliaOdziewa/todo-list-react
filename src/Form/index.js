9fc21f8873e66ad5efb1196337242c8e512e1009
JuliaOdziewa <78729930+JuliaOdziewa@users.noreply.github.com>
2021-12-30 22:22:05 +0100

    added focus on input after submit

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Button, Input, StyledForm } from "./styled";
 
 const Form = ({ addNewTask }) => {
@@ -17,14 +17,25 @@ const Form = ({ addNewTask }) => {
         setNewTaskContent("");
     };
 
+    const inputRef = useRef(null);
+
+    const inputFocus = () => {
+        inputRef.current.focus();
+    };
+
     return (
         <StyledForm onSubmit={onFormSubmit}>
             <Input
                 value={newTaskContent}
                 placeholder="Co jest do zrobienia?"
                 onChange={({ target }) => setNewTaskContent(target.value)}
+                ref={inputRef}
             />
-            <Button>Dodaj zadanie</ Button>
+            <Button
+                onClick={inputFocus}
+            >
+                Dodaj zadanie
+            </ Button>
         </StyledForm>
     )
 };
